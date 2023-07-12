dd41485313dec386c2178f2caff426d5907194ac
Erin Clark <erincodepirate@gmail.com>
2023-07-11 19:14:48 -0700

    Refactor, add display text

--- app/src/main/java/com/cursedfood/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/cursedfood/jetbizcard/MainActivity.kt
@@ -7,8 +7,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,16 +20,17 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cursedfood.jetbizcard.ui.theme.JetBizCardTheme
 
@@ -38,14 +40,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetBizCardTheme {
                 // A surface container using the 'background' color from the theme
-                CreateBizCard();
+                CreateBizCard()
             }
         }
     }
 }
 
 @Composable
-fun CreateBizCard(modifier: Modifier = Modifier) {
+fun CreateBizCard() {
     Surface(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -61,26 +63,61 @@ fun CreateBizCard(modifier: Modifier = Modifier) {
             ),
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
         ) {
-            Surface(modifier = Modifier
-                .size(150.dp)
-                .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp,Color.LightGray),
-                shadowElevation = 4.dp,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha=0.5f)
-            ) {
-                val imageModifier = Modifier
-                    .size(150.dp)
-                    .border(BorderStroke(1.dp, Color.Black))
-                    .background(Color.Yellow)
-                Image(
-                    painter = painterResource(id = R.drawable.racctrash),
-                    contentDescription = "raccoon",
-                    contentScale = ContentScale.Crop,
-                    modifier = imageModifier
-                )
-            }
+            DisplayInfo()
         }
+    }
+}
+
+@Composable
+private fun DisplayInfo() {
+    Column(
+        modifier = Modifier
+            .height(300.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DisplayProfileImage()
+        Divider(modifier = Modifier.padding(10.dp))
+        Column(modifier = Modifier.padding(5.dp)) {
+            Text(
+                text = "Raccy Raccoon",
+                style = MaterialTheme.typography.displayMedium,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            Text(
+                text = "Android Programmer",
+                modifier = Modifier.padding(3.dp)
+            )
+            Text(
+                text = "@raccytheraccoon",
+                modifier = Modifier.padding(3.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun DisplayProfileImage(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        val imageModifier = Modifier
+            .size(150.dp)
+            .border(BorderStroke(1.dp, Color.Black))
+            .background(Yellow)
+        Image(
+            painter = painterResource(id = R.drawable.racctrash),
+            contentDescription = "raccoon",
+            contentScale = ContentScale.Crop,
+            modifier = imageModifier
+        )
     }
 }
 
